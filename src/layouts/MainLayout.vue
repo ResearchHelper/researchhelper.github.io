5462992aa7ef4616a7e24625fc102ff98ca2d2bf
Hunt Feng <spacecurvature@gmail.com>
2023-04-02 11:50:12 -0600

    change to ts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="bg-dark">
+    <q-header elevated class="header text-white">
+      <q-toolbar>
+        <img src="~assets/logo.svg" alt="logo" />
+        <q-toolbar-title> Research Helper </q-toolbar-title>
+
+        <q-tabs v-model="tab" no-caps shrink stretch>
+          <q-route-tab
+            name="github"
+            icon="bi-github"
+            href="https://github.com/ResearchHelper/research-helper"
+            target="_blank"
+          />
+          <q-route-tab
+            name="download"
+            label="Download"
+            href="https://github.com/ResearchHelper/research-helper/releases"
+            target="_blank"
+          />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+});
+</script>
+
+<style>
+.header {
+  /* fuzzy glass */
+  background: #0000001a;
+  -webkit-backdrop-filter: blur(7px);
+  backdrop-filter: blur(7px);
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="column items-center">
+    <h2 class="text-white">
+      An efficient way to read papers and books and take notes
+    </h2>
+    <q-btn
+      square
+      no-caps
+      class="bg-primary text-white"
+      size="xl"
+      style="width: 30%"
+      label="Get Research Helper"
+      href="https://github.com/ResearchHelper/research-helper/releases"
+      target="_blank"
+    />
+
+    <q-card
+      dark
+      flat
+      style="width: 70%"
+      class="q-pa-md"
+    >
+      <q-card-section horizontal>
+        <q-img src="~assets/screenshot.png" />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
